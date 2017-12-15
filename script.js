98b34af29d888af2c8a5cc153e33bade901d97a6
benice195 <szczyrbaw@gmail.com>
2017-12-15 22:25:20 +0100

    Reuzywalnosc jednego komponentu

--- script.js
+++ script.js
@@ -55,38 +55,12 @@ var Counter = React.createClass({
   
 });
 
-var Counter2 = React.createClass({
-  getInitialState: function() {
-    return {
-      counter: 0,
-    };
-  },
-  increment: function() {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  },
-  decrement: function() {
-    this.setState({
-      counter: this.state.counter - 1,
-    });
-  },
-  render: function() {
-    return (
-      React.createElement('div', {}, 
-        React.createElement('span', {}, 'Licznik ' + this.state.counter),
-        React.createElement('button', {onClick: this.increment}, 'Increment'),
-        React.createElement('button', {onClick: this.decrement}, 'Decrement')
-      )
-    )
-  }
-});
 var App = React.createClass({
   render: function() {
     return (
       React.createElement('div', {},
         React.createElement(Counter, {}),
-        React.createElement(Counter2, {})
+        React.createElement(Counter, {})
       )
     );
   }
